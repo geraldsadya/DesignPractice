422b7ea5d3f3e8b8f0f31d59950dd4df4d9ccc72
Gerald Sadya <sadyageraldm@gmail.com>
2025-07-06 07:02:10 +0200

    Added className prop and use clsx + tailwind-merge in Button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { clsx } from "clsx";//clsx is a utility for constructing className strings conditionally
+import { twMerge } from "tailwind-merge";//tailwind-merge is a utility for merging Tailwind CSS class names, ensuring that conflicting classes are resolved correctly
+
 
 type ButtonProps = {//custom data structure that says "my button has these properties"
   children: React.ReactNode;      //anything inside <Button>...</Button>
-  onClick?: () => void;           
+  onClick?: () => void; 
+  className?: string;          
 };
 
 //the actual Button component
-export function Button({ children, onClick }: ButtonProps) {//create function and extract the properties it recieves
+export function Button({ children, onClick, className }: ButtonProps) {
   return (
     <button
-      onClick={onClick} //when the button is clicked, call the onClick function
-      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"// this is just to style the button
+      onClick={onClick}
+      className={twMerge(
+        clsx("bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition", className)//this is just to style the button with Tailwind CSS classes
+      )}
     >
       {children}
     </button>
